eac466212d2a9c734ad257e503b51e558aa17148
Rajesh Sharma <sharmarajesh42225@gmail.com>
2025-03-03 15:32:40 +0530

    data .json

---
+++ day3/p4.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs/promises');
+
+const server = http.createServer(async (req,res)=>{
+    res.setHeader('Content-type', 'application/json');
+    res.statusCode =200;
+    if (req.url==='/getdata' && req.method==='GET'){
+        const data = await fs.readFIle('/data.json', 'utf8');
+        res.end(JSON.stringify(data));
+    }
+    else if(req.url==='/setdata' && req.method==='POST'){
+        let body = '';
+        req.on('data',chunk =>{
+            bodt += chunk;
+        });
+        req.on('end', async()=>{
+            let users = [];
+            users = await fs.readFile('./data.json');
+            users = JSON.parse(users);
+            const newid = users.length>0?users[users.length-1].id+1:1;
+            const data= JSON.parse(body);
+            data.id = newid;
+            users.push(data);
+            await fs.writeFile('./data.json', JSON.stringify(users, null, 2));
+            res.end(JSON.stringify({message: 'Data saved'}));
+        });
+    }
+    else{}
+})
+
+server.listen(9001, () => {
+     console.log("Server runnign on port 9001");
+    //else console.log("Server started on port 9001");
+});
